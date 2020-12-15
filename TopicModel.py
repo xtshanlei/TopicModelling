ac0245d72535f07e9868c3bf93cf06bc6293274f
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-15 21:57:14 +0000

    Update TopicModel.py

--- TopicModel.py
+++ TopicModel.py
@@ -22,7 +22,7 @@ nltk.download('stopwords')
 nltk.download('punkt')
 from tqdm import tqdm_notebook as tqdm
 from send_email_attach import send_mail
-def download_link(object_to_download, download_filename, download_link_text):
+def download_link(object_to_download, download_filename):
     """
     Generates a link to download the given object_to_download.
 
