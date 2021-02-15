94fa6b8dc1fcd1dff304426a09368de4e0bab185
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-02-15 21:44:01 +0000

    Update TopicModel.py

--- TopicModel.py
+++ TopicModel.py
@@ -56,8 +56,7 @@ if uploaded_file:
     if merge_required:
         columns_selected = st.multiselect('Which columns do you want to merge?',df.columns)
         df['texts'] = df[columns_selected].agg(' '.join, axis=1)
-
-    st.write(df[text_column])
+        st.write(df[texts])
     ExStopWords = st.text_input("Any extra words to be removed? Split using space. e.g. good nice")
     ExStopWords_l = ExStopWords.split()
     stopset = stopwords.words('english') + ExStopWords_l
