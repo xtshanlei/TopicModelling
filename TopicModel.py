940217faf1c9c7beb0bc4fcb1f0dc3c581689112
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-02-15 17:37:08 +0000

    Update TopicModel.py

--- TopicModel.py
+++ TopicModel.py
@@ -97,8 +97,8 @@ if uploaded_file:
       if filtered in corpus:
         continue
       corpus.append(filtered)
-    st.write(corpus)
     vocab = sorted(list(vocab))
+    st.write(vocab)
     vocab_index = {}
     for i, w in enumerate(vocab):
         vocab_index[w] = i
