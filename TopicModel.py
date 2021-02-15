1db457352294b2760c1cde20883a58ecf5b013bc
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-02-15 21:40:36 +0000

    Update TopicModel.py

--- TopicModel.py
+++ TopicModel.py
@@ -51,14 +51,15 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file, encoding = 'ISO-8859-1', engine = 'python')
     df = df.dropna(how='all').replace(np.nan, '',regex=True).reset_index()
     st.write(df)
-    try: df = df['texts']
+    text_column = st.selectbox('Please choose the column name of the texts:',df.columns)
+    try: df = df[text_column]
     except:
         merge_required = st.checkbox('Need to merge columns?')
         if merge_required:
             columns_selected = st.multiselect('Which columns do you want to merge?',df.columns)
             df['texts'] = df[columns_selected].agg(' '.join, axis=1)
 
-    st.write(df['texts'])
+    st.write(df[text_column])
     ExStopWords = st.text_input("Any extra words to be removed? Split using space. e.g. good nice")
     ExStopWords_l = ExStopWords.split()
     stopset = stopwords.words('english') + ExStopWords_l
