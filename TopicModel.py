330449b7e826bd013ea5ec348a25063e3e929517
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-03-09 11:44:17 +0000

    fas

--- TopicModel.py
+++ TopicModel.py
@@ -179,6 +179,6 @@ if uploaded_file:
             tmp_download_link = download_link(results_df, 'h_topics.csv', 'Click here to download your data!')
             st.markdown(tmp_download_link, unsafe_allow_html=True)
     elif model_type == 'LDA':
-        st.write(LDA)
+        st.write('LDA')
     else:
         st.write('Please choose the topic model above!')
