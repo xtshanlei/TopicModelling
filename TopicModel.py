40f2da8df82eec32789f6e2aac6b7216919cdfd5
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-03-24 12:02:59 +0000

    sdfsdf

--- TopicModel.py
+++ TopicModel.py
@@ -224,13 +224,13 @@ if uploaded_file:
         id2word = corpora.Dictionary(corpus)
         data_lemmatized = corpus
         corpus = [id2word.doc2bow(text) for text in corpus]
-        st.subheader('Please choose the parameter for coherence score below...')
+        st.subheader('Please choose the parameters for LDA')
         topic_range = st.slider('Select a range of the topic number:', 2,30, (2,20),step=5)
         limit=topic_range[1]
         step=st.slider('Steps', 1, 5, value =5, step=1)
         start=topic_range[0]
 
-        start_coherence = st.button('Press to start LDA automatically..')
+        start_coherence = st.button('Press to start LDA automatically')
         if start_coherence:
             st.info('It may take a long time, please wait...')
 
